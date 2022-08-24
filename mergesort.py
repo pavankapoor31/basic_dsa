35f396a50e99675f1f8893085d57f43146379236
Pavan Kapoor <pavankapoor31@gmail.com>
2022-08-24 12:35:16 +0530

    added algorithm

---
+++ mergesort.py
@@ -0,0 +1,31 @@
+def merge_sort(nums):
+    # Terminating condition (list of 0 or 1 elements)
+    if len(nums) <= 1:
+        return nums
+    mid = len(nums) // 2
+    # Split the list into two halves
+    left = nums[:mid]
+    right = nums[mid:]
+    # Solve the problem for each half recursively
+    left_sorted, right_sorted = merge_sort(left), merge_sort(right)
+    # Combine the results of the two halves
+    sorted_nums =  merge(left_sorted, right_sorted)
+    
+    return sorted_nums
+def merge(nums1, nums2):     
+    merged = []
+    i, j = 0, 0
+    while i < len(nums1) and j < len(nums2):        
+        # Include the smaller element in the result and move to next element
+        if nums1[i] <= nums2[j]:
+            merged.append(nums1[i])
+            i += 1 
+        else:
+            merged.append(nums2[j])
+            j += 1
+    # Get the remaining parts
+    nums1_tail = nums1[i:]
+    nums2_tail = nums2[j:]
+    # Return the final merged array
+    return merged + nums1_tail + nums2_tail
+merge_sort([5,6,3,2,1,9,3])
--- winner.cpp
+++ winner.cpp
@@ -4,23 +4,34 @@ using namespace std;
 int main()
 {
     int n;
+    n=11;
     char arr[n] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k'};
     int count = 1;
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i <=n; i++)
     {
-
         count++;
         if (count == 5)
             count = 1;
         if (count == 4)
-        {
-            if (count + 1 < n)
+        {   
+            if ((i + 1)< n)
             {
-                arr[count] = arr[count + 1];
+                for(int k=i; k<n-1;k++){
+                arr[k] = arr[k + 1];
+                }
             }
-            else
-                n--;
+            n--;
+            
+        }
+        if(i==n)
+            i=0;
+        if(n==1)
+        {
+            cout<<arr[1];
+            break;
         }
     }
-    cout<<arr[];
+    
+
+    // cout<<arr[n]<<endl;
 }
